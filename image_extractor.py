8b60529a4b20e1aa7cecc99b13ff1a42a0576a77
Rémi TROTEL <remi.trotel@epita.fr>
2023-02-01 13:45:10 +0100

    added the extraction part

--- image_extractor.py
+++ image_extractor.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import time
 import cv2
 
@@ -19,28 +20,41 @@ class Program:
 
         # Create the folders if they don't already exists
         for folder in (self.input_folder, self.output_folder):
-            if (not os.path.isdir(self.input_folder)):
-                FileService.create_folder(self.input_folder)
+            if (not os.path.isdir(folder)):
+                # Create the folder inside the current working directory
+                FileService.create_folder(os.getcwd(), folder)
 
         # Find all videos from the input folder
         for file in os.listdir(self.input_folder):
+            filename, extension = os.path.splitext(file)
+
             # First check if the file is supported
-            if (not FileService.has_extension(file, ".mp4")):
+            if (not extension in (".mp4")):
                 print(f"{file} is not a supported type for the extraction, ignoring")
                 continue
 
             # We use the name of the file to create the folder
-            destination_folder = os.path.join(self.output_folder, file)
+            destination_folder = os.path.join(self.output_folder, filename)
 
             # If the folder already exists, ignore this video (already processed)
             if (os.path.isdir(destination_folder)):
+                skipped += 1
                 print(f"{file} has already been processed, skipping")
                 continue
 
             # The folder is created
-            FileService.create_folder(self.output_folder, file)
+            FileService.create_folder(self.output_folder, filename)
             # Then the video is processed
-            ImageExtractor.Extract(file, destination_folder)
+            try:
+                ImageExtractor.Extract(os.path.join(self.input_folder, file), filename, destination_folder)
+            except Exception as excpetion:
+                # Couldn't extract the video
+                print(f"{filename}: {excpetion}, skipping")
+                FileService.delete_folder(os.path.join(self.output_folder, filename))
+                skipped += 1
+            else:
+                # Extraction went well
+                processed += 1
 
         # End of the program
         print(f"Program exited successfully: {processed} processed, {skipped} skipped in {time.time() - start_time}s")
@@ -58,14 +72,29 @@ class FileService:
     def create_folder(folder_path, folder_name):
         os.mkdir(f"{folder_path}\{folder_name}")
 
+    @staticmethod
+    def delete_folder(folder_path):
+        shutil.rmtree(folder_path, ignore_errors=True)
+
 class ImageExtractor:
 
     @staticmethod
-    def Extract(video_name, folder):
+    def Extract(video_path, video_name, output_folder):
         """
         Extract the frames from the given video and store them in the given folder
         """
-        pass
+        # Open the video
+        video = cv2.VideoCapture(video_path)
+        # Check if the video has been opened correctly
+        if not video.isOpened():
+            raise Exception("An error as occured when trying to open the video")
+        # Get the number of frames and the framerate of the video
+        frame_number = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = video.get(cv2.CAP_PROP_FPS)
+        # Convert all frames into an image
+        for i in range(frame_number):
+            success, frame = video.read()
+            cv2.imwrite(os.path.join(output_folder, f"{video_name}_{i:0<5}.jpg"), frame)
 
 if __name__ == "__main__":
     Program(INPUT_FOLDER, OUTPUT_FOLDER).main()
